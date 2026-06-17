48b63d04a3fce5d481963eb361e8489400cd8e33
Sarvesh Maurya <sarveshmaurya7307@gmail.com>
2026-06-17 06:21:46 +0530

    Added new Java programs

--- src/OOPs/StudentClass.java
+++ src/OOPs/StudentClass.java
@@ -1,33 +1,33 @@
 package OOPs;
 
 public class StudentClass {
-//    public static class Student{
-//        // creating a new data type
-//
-//        String name ;
-//       private int roll_no;
-//        double percentage;
-//    }
-//    public static class Car{
-//        String name;
-//        String type;
-//        int price;
-//    }
+    public static class Student{
+        // creating a new data type
+
+        String name ;
+       private int rno;
+        double percent;
+    }
+    public static class Car{
+        String name;
+        String type;
+        int price;
+    }
     public static void main(String[] args) {
         Student x = new Student();    // declaration
         x.name ="Sarvesh";
-        x.roll_no = 46;
-        x.percentage = 98.4;
+        x.rno = 46;
+        x.percent = 98.4;
 //        System.out.println(x.name);
 //        System.out.println(x.roll_no + 4);
 
         Student s = new Student();
         s.name= "Azad";
-        s.roll_no= 59;
-        s.percentage= 75;
-        System.out.println(s.getRoll_no());
-        s.setRoll_no(67);
-//        System.out.println(s.roll_no);
+        s.rno= 59;
+        s.percent= 75;
+//        System.out.println(s.getRno());
+//        s.setRno(67);
+//        System.out.println(s.getRno());
 
 
     }
---
+++ src/OOPs/Students.java
@@ -0,0 +1,39 @@
+package OOPs;
+
+public class Students {
+        // creating a new data type
+       public String name;
+         int rno;
+        double percent;
+        final String schoolName="RPM";
+
+        // default constructor
+    public Students(){
+
+    }
+
+        public Students(String naam, int roll, double per){  // constructor
+            name=naam;
+            rno=roll;
+            percent=per;
+
+
+        }
+
+        public int getRno(){  // getter
+            return rno;
+        }
+
+//        public void setRno(int roll){  //setter
+//            rno=roll;
+//
+//    }
+    public void setRno(int rno){  // this
+            this.rno=rno;
+    }
+
+    // Constructor-
+    // student s1 = new student ("sarvesh",67,96.3);
+
+
+}
