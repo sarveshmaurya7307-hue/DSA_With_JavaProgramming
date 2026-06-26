a97ba4c8ddd811e7621df8c865f56f48ce721b71
Sarvesh Maurya <sarveshmaurya7307@gmail.com>
2026-06-26 20:00:47 +0530

    add new java program

--- src/MergeSorting/InversionCount.java
+++ src/MergeSorting/InversionCount.java
@@ -70,3 +70,82 @@ public class InversionCount {
 
     }
 }
+
+
+
+// Reverse Pairs leetcode problem no. 493
+
+class Solution {
+    static int count=0;
+    public  void print(int[] arr) {
+        for(int ele : arr){
+            System.out.print(ele+" ");
+        }
+        System.out.println();
+
+    }
+    public  void inversion(int[] a, int[] b){
+        int i=0, j=0;
+        while(i<a.length && j<b.length){
+            if((long)a[i]>(long)2*(long)b[j]){
+                count +=(a.length-i);
+                j++;
+            }
+            else i++;
+        }
+
+    }
+    public  void merge(int[] a, int[] b, int[] c){
+        int i=0, j=0, k=0;
+        while(i<a.length && j<b.length){
+            if(a[i]<=b[j]) {
+                c[k++] = a[i++];
+            }
+            else {
+                c[k++]=b[j++];
+            }
+        }
+        while(j<b.length) c[k++]=b[j++];
+        while(i<a.length) c[k++]=a[i++];
+    }
+    public  void mergesort(int[]arr){
+        int n =arr.length;
+        if(n==1) return;  // base case
+
+        // create two arrays of n/2 size each
+        int[] a = new int[n/2];
+        int[] b = new int[n-n/2];
+
+        // copy pasting
+        for(int i=0;i<n/2;i++){
+            a[i]=arr[i];
+        }
+        for(int i=0;i<n-n/2;i++){
+            b[i]=arr[i+n/2];
+        }
+        // mergesort karna hai ab
+        mergesort(a);
+        mergesort(b);
+//        // find inversion count
+        inversion(a,b);
+        // merge these 'a' and 'b'
+        merge(a,b,arr);
+        // delete of a & b
+        a=null; b=null;
+
+    }
+    public void main(String[] args) {
+        int[] arr = {80,10,70,30,60,40,50,20};
+        print(arr);
+        mergesort(arr);
+        print(arr);
+        System.out.println(count);
+
+    }
+    public int reversePairs(int[] nums) {
+        count =0;
+        mergesort(nums);
+        return count;
+
+    }
+}
